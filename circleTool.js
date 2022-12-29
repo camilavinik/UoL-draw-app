750d170e17944c907f364387c203a3955270bb6b
Camila <contactcamilasanchez@gmail.com>
2022-12-29 19:47:07 -0300

    formatted circle and rect files and added icon

--- circleTool.js
+++ circleTool.js
@@ -1,49 +1,41 @@
 //a tool for drawing circles to the screen. Allows the user to preview
-//the circle to the current mouse position before drawing the circle to the 
+//the circle to the current mouse position before drawing the circle to the
 //pixel array.
-function CircleTool(){
-	this.icon = "assets/lineTo.jpg";
-	this.name = "Circle";
+function CircleTool() {
+  this.icon = "assets/circle.jpg";
+  this.name = "Circle";
 
-	var startMouseX = -1;
-	var startMouseY = -1;
-	var drawing = false;
+  let startMouseX = -1;
+  let startMouseY = -1;
+  let drawing = false;
 
-	//draws the circle to the screen 
-	this.draw = function(){
-
-		//only draw when mouse is clicked
-		if(mouseIsPressed){
-			//if it's the start of drawing a new circle
-			if(startMouseX == -1){
-				startMouseX = mouseX;
-				startMouseY = mouseY;
-				drawing = true;
-				//save the current pixel Array
-				loadPixels();
-			}
-
-			else{
-				//update the screen with the saved pixels to hide any previous
-				//circle between mouse pressed and released
-				updatePixels();
-				//draw the circle
-				const width = (mouseX - startMouseX) * 2
-				const height = (mouseY - startMouseY) * 2
-				ellipse(startMouseX, startMouseY, width, height);
-			}
-
-		}
-
-		else if(drawing){
-			//save the pixels with the most recent circle and reset the
-			//drawing bool and start locations
-			loadPixels();
-			drawing = false;
-			startMouseX = -1;
-			startMouseY = -1;
-		}
-	};
-
-
+  //draws the circle to the screen
+  this.draw = function () {
+    //only draw when mouse is clicked
+    if (mouseIsPressed) {
+      //if it's the start of drawing a new circle
+      if (startMouseX == -1) {
+        startMouseX = mouseX;
+        startMouseY = mouseY;
+        drawing = true;
+        //save the current pixel Array
+        loadPixels();
+      } else {
+        //update the screen with the saved pixels to hide any previous
+        //circle between mouse pressed and released
+        updatePixels();
+        //draw the circle
+        const width = (mouseX - startMouseX) * 2;
+        const height = (mouseY - startMouseY) * 2;
+        ellipse(startMouseX, startMouseY, width, height);
+      }
+    } else if (drawing) {
+      //save the pixels with the most recent circle and reset the
+      //drawing bool and start locations
+      loadPixels();
+      drawing = false;
+      startMouseX = -1;
+      startMouseY = -1;
+    }
+  };
 }
--- rectangleTool.js
+++ rectangleTool.js
@@ -1,49 +1,41 @@
 //a tool for drawing rectangles to the screen. Allows the user to preview
-//the rectangle to the current mouse position before drawing the rectangle to the 
+//the rectangle to the current mouse position before drawing the rectangle to the
 //pixel array.
-function RectangleTool(){
-	this.icon = "assets/lineTo.jpg";
-	this.name = "rectangle";
+function RectangleTool() {
+  this.icon = "assets/rectangle.jpg";
+  this.name = "rectangle";
 
-	var startMouseX = -1;
-	var startMouseY = -1;
-	var drawing = false;
+  let startMouseX = -1;
+  let startMouseY = -1;
+  let drawing = false;
 
-	//draws the rectangle to the screen 
-	this.draw = function(){
-
-		//only draw when mouse is clicked
-		if(mouseIsPressed){
-			//if it's the start of drawing a new rectangle
-			if(startMouseX == -1){
-				startMouseX = mouseX;
-				startMouseY = mouseY;
-				drawing = true;
-				//save the current pixel Array
-				loadPixels();
-			}
-
-			else{
-				//update the screen with the saved pixels to hide any previous
-				//rectangle between mouse pressed and released
-				updatePixels();
-				//draw the rectangle
-				const width = (mouseX - startMouseX)
-				const height = (mouseY - startMouseY)
-				rect(startMouseX, startMouseY, width, height);
-			}
-
-		}
-
-		else if(drawing){
-			//save the pixels with the most recent rectangle and reset the
-			//drawing bool and start locations
-			loadPixels();
-			drawing = false;
-			startMouseX = -1;
-			startMouseY = -1;
-		}
-	};
-
-
+  //draws the rectangle to the screen
+  this.draw = function () {
+    //only draw when mouse is clicked
+    if (mouseIsPressed) {
+      //if it's the start of drawing a new rectangle
+      if (startMouseX == -1) {
+        startMouseX = mouseX;
+        startMouseY = mouseY;
+        drawing = true;
+        //save the current pixel Array
+        loadPixels();
+      } else {
+        //update the screen with the saved pixels to hide any previous
+        //rectangle between mouse pressed and released
+        updatePixels();
+        //draw the rectangle
+        const width = mouseX - startMouseX;
+        const height = mouseY - startMouseY;
+        rect(startMouseX, startMouseY, width, height);
+      }
+    } else if (drawing) {
+      //save the pixels with the most recent rectangle and reset the
+      //drawing bool and start locations
+      loadPixels();
+      drawing = false;
+      startMouseX = -1;
+      startMouseY = -1;
+    }
+  };
 }

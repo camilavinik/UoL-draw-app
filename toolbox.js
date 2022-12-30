c490e75311f0f5fc4149e890016288a6ab105b1e
Camila <contactcamilasanchez@gmail.com>
2022-12-29 21:56:24 -0300

    fix mirror

--- toolbox.js
+++ toolbox.js
@@ -1,82 +1,87 @@
 //container object for storing the tools. Functions to add new tools and select a tool
 function Toolbox() {
+  let self = this;
 
-	var self = this;
+  this.tools = [];
+  this.selectedTool = null;
 
-	this.tools = [];
-	this.selectedTool = null;
+  let toolbarItemClick = function () {
+    //remove any existing borders
+    let items = selectAll(".sideBarItem");
+    for (let i = 0; i < items.length; i++) {
+      items[i].style("border", "0");
+    }
 
-	var toolbarItemClick = function() {
-		//remove any existing borders
-		var items = selectAll(".sideBarItem");
-		for (var i = 0; i < items.length; i++) {
-			items[i].style('border', '0')
-		}
+    let toolName = this.id().split("sideBarItem")[0];
+    self.selectTool(toolName);
 
-		var toolName = this.id().split("sideBarItem")[0];
-		self.selectTool(toolName);
+    //call loadPixels to make sure most recent changes are saved to pixel array
+    loadPixels();
+  };
 
-		//call loadPixels to make sure most recent changes are saved to pixel array
-		loadPixels();
+  //add a new tool icon to the html page
+  let addToolIcon = function (icon, name) {
+    let sideBarItem = createDiv("<img src='" + icon + "'></div>");
+    sideBarItem.class("sideBarItem");
+    sideBarItem.id(name + "sideBarItem");
+    sideBarItem.parent("sidebar");
+    sideBarItem.mouseClicked(toolbarItemClick);
+  };
 
-	}
+  //add a tool to the tools array
+  this.addTool = function (tool) {
+    //check that the object tool has an icon and a name
+    if (!tool.hasOwnProperty("icon") || !tool.hasOwnProperty("name")) {
+      alert("make sure your tool has both a name and an icon");
+    }
+    this.tools.push(tool);
+    addToolIcon(tool.icon, tool.name);
+    //if no tool is selected (ie. none have been added so far)
+    //make this tool the selected one.
+    if (this.selectedTool == null) {
+      this.selectTool(tool.name);
+    }
+  };
 
-	//add a new tool icon to the html page
-	var addToolIcon = function(icon, name) {
-		var sideBarItem = createDiv("<img src='" + icon + "'></div>");
-		sideBarItem.class('sideBarItem')
-		sideBarItem.id(name + "sideBarItem")
-		sideBarItem.parent('sidebar');
-		sideBarItem.mouseClicked(toolbarItemClick);
+  this.selectTool = function (toolName) {
+    //search through the tools for one that's name matches
+    //toolName
+    for (let i = 0; i < this.tools.length; i++) {
+      if (this.tools[i].name == toolName) {
+        //if the tool has an unselectTool method run it.
+        if (
+          this.selectedTool != null &&
+          this.selectedTool.hasOwnProperty("unselectTool")
+        ) {
+          this.selectedTool.unselectTool();
+        }
+        //select the tool and highlight it on the toolbar
+        this.selectedTool = this.tools[i];
+        select("#" + toolName + "sideBarItem").style(
+          "border",
+          "2px solid blue"
+        );
 
+        //if the tool has an options area. Populate it now.
+        if (this.selectedTool.hasOwnProperty("populateOptions")) {
+          this.selectedTool.populateOptions();
+        }
+      }
+    }
+  };
 
-	};
+  this.unselectTool = function () {
+    //remove any existing borders
+    let items = selectAll(".sideBarItem");
+    for (let i = 0; i < items.length; i++) {
+      items[i].style("border", "0");
+    }
 
-	//add a tool to the tools array
-	this.addTool = function(tool) {
-		//check that the object tool has an icon and a name
-		if (!tool.hasOwnProperty("icon") || !tool.hasOwnProperty("name")) {
-			alert("make sure your tool has both a name and an icon");
-		}
-		this.tools.push(tool);
-		addToolIcon(tool.icon, tool.name);
-		//if no tool is selected (ie. none have been added so far)
-		//make this tool the selected one.
-		if (this.selectedTool == null) {
-			this.selectTool(tool.name);
-		}
-	};
-
-	this.selectTool = function(toolName) {
-		//search through the tools for one that's name matches
-		//toolName
-		for (var i = 0; i < this.tools.length; i++) {
-			if (this.tools[i].name == toolName) {
-				//if the tool has an unselectTool method run it.
-				if (this.selectedTool != null && this.selectedTool.hasOwnProperty(
-						"unselectTool")) {
-					this.selectedTool.unselectTool();
-				}
-				//select the tool and highlight it on the toolbar
-				this.selectedTool = this.tools[i];
-				select("#" + toolName + "sideBarItem").style("border", "2px solid blue");
-
-				//if the tool has an options area. Populate it now.
-				if (this.selectedTool.hasOwnProperty("populateOptions")) {
-					this.selectedTool.populateOptions();
-				}
-			}
-		}
-	};
-
-	this.unselectTool = function() {
-		//remove any existing borders
-		var items = selectAll(".sideBarItem");
-		for (var i = 0; i < items.length; i++) {
-			items[i].style('border', '0')
-		}
-
-		// remove selected tool
-		this.selectedTool = null
-	}
+    //if the tool has an unselectTool method run it.
+    if (this.selectedTool?.hasOwnProperty("unselectTool")) {
+      this.selectedTool.unselectTool();
+    }
+    // remove selected tool
+    this.selectedTool = null;
+  };
 }
